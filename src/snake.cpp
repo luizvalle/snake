714222cea7520655f0a25ea9182e445c4b9586e4
luizvalle <lf.lemos.valle@gmail.com>
2023-03-19 22:59:47 -0400

    Increase starting snake size and add outline color to differentiate segments

--- src/snake.cpp
+++ src/snake.cpp
@@ -6,10 +6,13 @@
 
 using namespace snake;
 
-Snake::Snake(int x_pos, int y_pos)
+Snake::Snake(int x_pos, int y_pos, size_t length)
 {
-    Snake::Segment segment {x_pos, y_pos};
-    segments.push_front(std::move(segment));
+    while (length--) {
+        Snake::Segment segment {x_pos, y_pos};
+        segments.push_front(std::move(segment));
+        x_pos -= segment.get_length();
+    }
 }
 
 void Snake::render(SDL_Renderer *renderer) const
@@ -20,11 +23,15 @@ void Snake::render(SDL_Renderer *renderer) const
 }
 
 Snake::Segment::Segment(int x_pos, int y_pos)
-    : square{ x_pos, y_pos, 10, 10 } {}
+    : square{x_pos, y_pos, 10, 10} {}
 
 void Snake::Segment::render(SDL_Renderer *renderer) const
 {
     SDL_SetRenderDrawColor(
-            renderer, color.r, color.g, color.b, color.a); 
+            renderer, body_color.r, body_color.g, body_color.b, body_color.a); 
     SDL_RenderFillRect(renderer, &square);
+    SDL_SetRenderDrawColor(
+            renderer, outline_color.r, outline_color.g, outline_color.b,
+            outline_color.a); 
+    SDL_RenderDrawRect(renderer, &square);
 }
--- src/snake.hpp
+++ src/snake.hpp
@@ -10,16 +10,18 @@
 namespace snake {
     class Snake final : public GraphicalElement {
         public:
-            Snake(int x_pos, int y_pos);
+            Snake(int x_pos, int y_pos, size_t length = 3);
             void render(SDL_Renderer *renderer) const override;
         private:
             class Segment final : public GraphicalElement {
                 public:
                     Segment(int x_pos, int y_pos);
+                    size_t get_length() const { return square.w; }
                     void render(SDL_Renderer *renderer) const override;
                 private:
                     SDL_Rect square;
-                    SDL_Color color {2, 100, 64, 255};
+                    SDL_Color body_color {2, 100, 64, 255};
+                    SDL_Color outline_color {0, 0, 0, 0};
             };
             std::forward_list<Segment> segments;
             SDL_Renderer *renderer;
