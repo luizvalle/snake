d08dcb80e648185cad1e83995cef44fb107b3995
luizvalle <lf.lemos.valle@gmail.com>
2023-03-21 23:59:54 -0400

    Changed usages of 'using namespace' for namespace

--- src/snake.cpp
+++ src/snake.cpp
@@ -4,77 +4,77 @@
 #include <SDL2/SDL.h>
 #include "snake.hpp"
 
-using namespace snake;
-
-Snake::Snake(int x_pos, int y_pos, size_t length, int w)
-    : width{w}, speed{w}
-{
-    while (length--) {
-        Snake::Segment segment {x_pos, y_pos, width};
-        segments.push_back(std::move(segment));
-        x_pos -= width;
+namespace snake {
+    Snake::Snake(int x_pos, int y_pos, size_t length, int w)
+        : width{w}, speed{w}
+    {
+        while (length--) {
+            Snake::Segment segment {x_pos, y_pos, width};
+            segments.push_back(std::move(segment));
+            x_pos -= width;
+        }
     }
-}
 
-void Snake::change_direction(Direction direction)
-{
-    switch (direction) {
-        case UP:
-            x_vel = 0;
-            y_vel = -speed;
-            break;
-        case DOWN:
-            x_vel = 0;
-            y_vel = speed;
-            break;
-        case LEFT:
-            x_vel = -speed;
-            y_vel = 0;
-            break;
-        case RIGHT:
-            x_vel = speed;
-            y_vel = 0;
-            break;
+    void Snake::change_direction(Direction direction)
+    {
+        switch (direction) {
+            case UP:
+                x_vel = 0;
+                y_vel = -speed;
+                break;
+            case DOWN:
+                x_vel = 0;
+                y_vel = speed;
+                break;
+            case LEFT:
+                x_vel = -speed;
+                y_vel = 0;
+                break;
+            case RIGHT:
+                x_vel = speed;
+                y_vel = 0;
+                break;
+        }
     }
-}
 
-void Snake::move()
-{
-    auto segment = segments.begin();
-    int prev_x = segment->get_x(), prev_y = segment->get_y();
-    segment->set_x(prev_x + x_vel);
-    segment->set_y(prev_y + y_vel);
-    while (++segment != segments.end()) {
-        int tmp_x = segment->get_x(), tmp_y = segment->get_y();
-        segment->set_x(prev_x);
-        segment->set_y(prev_y);
-        prev_x = tmp_x;
-        prev_y = tmp_y;
+    void Snake::move()
+    {
+        auto segment = segments.begin();
+        int prev_x = segment->get_x(), prev_y = segment->get_y();
+        segment->set_x(prev_x + x_vel);
+        segment->set_y(prev_y + y_vel);
+        while (++segment != segments.end()) {
+            int tmp_x = segment->get_x(), tmp_y = segment->get_y();
+            segment->set_x(prev_x);
+            segment->set_y(prev_y);
+            prev_x = tmp_x;
+            prev_y = tmp_y;
+        }
     }
-}
 
-void Snake::render(SDL_Renderer *renderer) const
-{
-    std::for_each(segments.begin(),
-                  segments.end(),
-                  std::bind(&Segment::render, std::placeholders::_1, renderer));
-}
+    void Snake::render(SDL_Renderer *renderer) const
+    {
+        std::for_each(segments.begin(),
+                segments.end(),
+                std::bind(&Segment::render, std::placeholders::_1, renderer));
+    }
 
-Snake::Segment::Segment(int x_pos, int y_pos, int width)
-    : segment{x_pos, y_pos, width, width} {}
+    Snake::Segment::Segment(int x_pos, int y_pos, int width)
+        : segment{x_pos, y_pos, width, width} {}
 
-void Snake::Segment::render(SDL_Renderer *renderer) const
-{
-    SDL_SetRenderDrawColor(renderer,
-                           fill_color.r,
-                           fill_color.g,
-                           fill_color.b,
-                           fill_color.a);
-    SDL_RenderFillRect(renderer, &segment);
-    SDL_SetRenderDrawColor(renderer,
-                           outline_color.r,
-                           outline_color.g,
-                           outline_color.b,
-                           outline_color.a);
-    SDL_RenderDrawRect(renderer, &segment);
+    void Snake::Segment::render(SDL_Renderer *renderer) const
+    {
+        SDL_SetRenderDrawColor(renderer,
+                fill_color.r,
+                fill_color.g,
+                fill_color.b,
+                fill_color.a);
+        SDL_RenderFillRect(renderer, &segment);
+        SDL_SetRenderDrawColor(renderer,
+                outline_color.r,
+                outline_color.g,
+                outline_color.b,
+                outline_color.a);
+        SDL_RenderDrawRect(renderer, &segment);
+    }
 }
--- src/window.cpp
+++ src/window.cpp
@@ -4,35 +4,35 @@
 #include <SDL2/SDL.h>
 #include "window.hpp"
 
-using namespace snake;
+namespace snake {
+    Window::Window(int width, int height, std::string name)
+    {
+        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)) {
+            // TODO: Error
+        }
 
-Window::Window(int width, int height, std::string name)
-{
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)) {
-    // TODO: Error
-  }
+        window = SDL_CreateWindow(name.c_str(),
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                width, height, 0);
 
-  window = SDL_CreateWindow(name.c_str(),
-                            SDL_WINDOWPOS_CENTERED,
-                            SDL_WINDOWPOS_CENTERED,
-                            width, height, 0);
+        if (!window) {
+            // TODO: Error
+        }
 
-  if (!window) {
-    // TODO: Error
-  }
+        uint32_t render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
 
-  uint32_t render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+        renderer = SDL_CreateRenderer(window, -1, render_flags);
 
-  renderer = SDL_CreateRenderer(window, -1, render_flags);
+        if (!renderer) {
+            // TODO: Error
+        }
+    }
 
-  if (!renderer) {
-    // TODO: Error
-  }
-}
-
-Window::~Window()
-{
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    Window::~Window()
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
 }
